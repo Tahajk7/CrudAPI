dde00a9a96df89a2f8b4efe01c7179cc824d5fa1
Tahajk7 <taha.celik2509@gmail.com>
2023-08-31 11:38:09 +0300

    postman işlemleri yapıldı

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,36 @@
 import express from "express";
-
 const router = express.Router();
+import { v4 as uuidv4 } from "uuid";
 
 const users = [
-  {
-    Firstname: "Taha",
-    LastName: "Çelik",
-    age: 17,
-  },
+  //   {
+  //     Firstname: "Taha",
+  //     LastName: "Çelik",
+  //     age: 17,
+  //   },
+  //   {
+  //     Firstname: "Emre",
+  //     LastName: "Koç",
+  //     age: 18,
+  //   },
 ];
 
 router.get("/", (req, res) => {
-  res.send("Hello");
-  console.log(users);
+  res.send(users);
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+
+  console.log(users, user);
+
+  users.push({ ...user, id: uuidv4() });
+
+  res.send(`User with the name ${user.Firstname} added to the database!`);
+});
+
+router.get("/:id", (req, res) => {
+  res.send("Henüz bir ıd almadınız");
 });
 
 export default router;
